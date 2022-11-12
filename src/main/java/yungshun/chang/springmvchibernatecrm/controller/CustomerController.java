2e3088353df61873576dcee1d81a99e0157a4b5d
yungshun317 <seraph317@gmail.com>
2022-11-12 10:17:32 +0800

    [FEAT] Create the `Customer` entity class

--- src/main/java/yungshun/chang/springmvchibernatecrm/controller/CustomerController.java
+++ src/main/java/yungshun/chang/springmvchibernatecrm/controller/CustomerController.java
@@ -1,4 +1,4 @@
-package yungshun.chang.springmvchibernatecrm;
+package yungshun.chang.springmvchibernatecrm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
