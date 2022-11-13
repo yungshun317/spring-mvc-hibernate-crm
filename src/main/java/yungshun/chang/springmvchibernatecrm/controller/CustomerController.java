3a7c4169eb880bd2da52925feddc9f4607651789
yungshun317 <seraph317@gmail.com>
2022-11-13 10:58:01 +0800

    [FEAT] Inject the `CustomerDAO` and get customers from the DAO

--- src/main/java/yungshun/chang/springmvchibernatecrm/controller/CustomerController.java
+++ src/main/java/yungshun/chang/springmvchibernatecrm/controller/CustomerController.java
@@ -1,15 +1,31 @@
 package yungshun.chang.springmvchibernatecrm.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import yungshun.chang.springmvchibernatecrm.dao.CustomerDAO;
+import yungshun.chang.springmvchibernatecrm.entity.Customer;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+    // Need to inject the customer DAO
+    @Autowired
+    private CustomerDAO customerDAO;
+
     @RequestMapping("/list")
     public String listCustomers(Model model) {
+
+        // Get customers from the DAO
+        List<Customer> customers = customerDAO.getCustomers();
+
+        // Add customers to the model
+        model.addAttribute("customers", customers);
+
         return "list-customers";
     }
 }
