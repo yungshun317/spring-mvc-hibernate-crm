ad9ca10cb9199f0aeac87cbc2bfd192baf10626c
yungshun317 <seraph317@gmail.com>
2022-11-16 08:36:11 +0800

    [FEAT] Update `CustomerController` for adding new customers

--- src/main/java/yungshun/chang/springmvchibernatecrm/controller/CustomerController.java
+++ src/main/java/yungshun/chang/springmvchibernatecrm/controller/CustomerController.java
@@ -3,9 +3,13 @@ package yungshun.chang.springmvchibernatecrm.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import yungshun.chang.springmvchibernatecrm.dao.CustomerDAO;
 import yungshun.chang.springmvchibernatecrm.entity.Customer;
+import yungshun.chang.springmvchibernatecrm.service.CustomerService;
 
 import java.util.List;
 
@@ -13,19 +17,39 @@ import java.util.List;
 @RequestMapping("/customer")
 public class CustomerController {
 
-    // Inject the customer DAO
+    // Inject the customer service
     @Autowired
-    private CustomerDAO customerDAO;
+    private CustomerService customerService;
 
     @RequestMapping("/list")
     public String listCustomers(Model model) {
 
         // Get customers from the DAO
-        List<Customer> customers = customerDAO.getCustomers();
+        List<Customer> customers = customerService.getCustomers();
 
         // Add customers to the model
         model.addAttribute("customers", customers);
 
         return "list-customers";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+
+        // Create model attribute to bind form data
+        Customer customer = new Customer();
+
+        model.addAttribute("customer", customer);
+
+        return "customer-form";
+    }
+
+    @PostMapping("/saveCustomer")
+    public String saveCustomer(@ModelAttribute("customer") Customer customer) {
+
+        // Save customer using service
+        customerService.saveCustomer(customer);
+
+        return "redirect:/customer/list";
     }
 }
