0f81caa78da96f7263db3f5e83ea4f13d4deee71
yungshun317 <seraph317@gmail.com>
2022-11-19 09:54:56 +0800

    [FEAT] Define `deleteCustomer` in `CustomerService` and `CustomerServiceImpl`

--- src/main/java/yungshun/chang/springmvchibernatecrm/dao/CustomerDAOImpl.java
+++ src/main/java/yungshun/chang/springmvchibernatecrm/dao/CustomerDAOImpl.java
@@ -5,7 +5,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import yungshun.chang.springmvchibernatecrm.entity.Customer;
 
 import java.util.List;
